2756b317d79d193f2b18ba6c96d8506fd7feb47c
pycoo01 <pycoo.man@gmail.com>
2025-11-10 12:23:31 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "foo" {
-  ami           = "ami-0ecb62995f68bb549" # us-west-1
+  ami           = "ami-0ecb62995f68bb549" # us-east-1
   instance_type = "t2.micro"
   tags = {
       Name = "TF-Instance"
